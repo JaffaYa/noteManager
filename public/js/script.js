22ccc49a9928c91c9995df8eaa78fd3febdaab22
Oleksandr_Plopan <jaffaya@gmail.com>
2021-12-24 18:27:06 +0200

    tick multiplier1

--- public/js/script.js
+++ public/js/script.js
@@ -367,15 +367,10 @@
 				var now = new Date();
 				var fps = (animFrameCount / (now - animStartTime))*1000;
 				if(fps < 30){
-					ticksPerRender = 2;
+					ticksPerRender++;
 			        animStartTime = now;animFrameCount = 0;  //Reset the fps counter
-			    }else if(fps < 15){
-			    	ticksPerRender = 4;
-			        animStartTime = now;animFrameCount = 0;  //Reset the fps counter
-			    }else if(fps < 8){
-			    	ticksPerRender = 8;
-			        animStartTime = now;animFrameCount = 0;  //Reset the fps counter
-			    }else if(fps > 60 && ticksPerRender > 1){
+			    }
+			    if(fps > 60 && ticksPerRender > 1){
 			    	ticksPerRender--;
 			        animStartTime = now;animFrameCount = 0;  //Reset the fps counter
 			    }
